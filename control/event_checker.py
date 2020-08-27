d6dbd31e200336d268b6073d0c92836ab2e063b9
daniel.bebber@gmx.de <daniel.bebber@gmx.de>
2020-08-27 20:15:20 +0200

    dev: add logging and eventloop to event checker
    * Add logging and eventloop to event checker

--- control/event_checker.py
+++ control/event_checker.py
@@ -6,12 +6,19 @@
 # Copyright: Daniel Bebber, 2020
 # Author: Daniel Bebber <daniel.bebber@gmx.de>
 # ----------------------------------------------
+import logging
 import time
 
 from control.database_controller import DatabaseController
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 
 class EventChecker:
+    """Checker for events."""
 
     def __init__(self, updater):
         """Constructor."""
@@ -21,10 +28,11 @@ class EventChecker:
         self.updater = updater
         self.interval = DatabaseController.configuration['configuration_values']['event_checker']['interval']
 
-        self.check_events()
-
     def check_events(self):
         """
         Returns:
 
         """
+        logger.info("pew pew")
+        time.sleep(self.interval)
+        self.check_events()
